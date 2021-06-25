379f8050b53c6f3e524bbcb5f20e0888d670d757
heunyam <tpwns072@gmail.com>
2021-06-26 02:08:34 +0900

    feat: Add router.js

---
+++ helog-front/src/router.js
@@ -0,0 +1,11 @@
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={} />
+      </Switch>
+    </BrowserRouter>
+  );
+}

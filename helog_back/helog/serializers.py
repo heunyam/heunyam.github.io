8d7e807091a35b1a237100f13df99696d616aa8a
heunyam <tpwns072@gmail.com>
2021-06-28 22:21:09 +0900

    refactor: PostsSerializer move to PostsAPI view class

--- helog_back/helog/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import PostsModel
-
-
-class PostsSerializer(ModelSerializer):
-    class Meta:
-        model = PostsModel
-        fields = '__all__'
--- helog_back/helog/views.py
+++ helog_back/helog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import (
+    views,
+    serializers,
+    status
+)
+from helog.models import PostsModel
 
-# Create your views here.
+
+class PostsAPI(views.APIView):
+
+    class PostsSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = PostsModel
+            fields = (
+                'title',
+                'content',
+                'created_datetime'
+            )
+
+    def get(self, request):
+        serializer = self.PostsSerializer(data=request.data)
+
+
+
+    def post(self, request):
+        return Response({
+            'ok': True
+        })

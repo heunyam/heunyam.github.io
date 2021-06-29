c90b80f3089fbef8a6d8df24ae94f8c142c987e9
heunyam <tpwns072@gmail.com>
2021-06-29 20:13:22 +0900

    !change: 요구 변경으로 인해 PostsModel 재정의

--- helog_back/helog/models.py
+++ helog_back/helog/models.py
@@ -1,12 +1,12 @@
 from django.db.models import (
     Model,
     CharField,
-    TextField,
     DateTimeField
 )
 
 
 class PostsModel(Model):
-    title = CharField(max_length=40)
-    content = TextField()
+    title = CharField(max_length=40, null=False)
+    subTitle = CharField(max_length=200, null=True)
+    link = CharField(max_length=200, null=False)
     created_datetime = DateTimeField(auto_now_add=True)

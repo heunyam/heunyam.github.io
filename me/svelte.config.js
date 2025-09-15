d625d2667d231aae2009f4747a4a13bec9cd24cc
heunyam <tpwns072@gmail.com>
2025-09-15 21:16:45 +0900

    chore: deploy setup

--- me/svelte.config.js
+++ me/svelte.config.js
@@ -2,7 +2,6 @@ import adapter from '@sveltejs/adapter-auto';
 import adapterStatic from '@sveltejs/adapter-static';
 
 const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-const repoName = "heunyam.github.io"
 
 const config = {
   kit: {
@@ -12,9 +11,13 @@ const config = {
           assets: 'build',
           fallback: null
         })
-      : adapter(), // 로컬 개발용
+      : adapter(),
     paths: {
-      base: isGitHubPages ? repoName : ''
+      base: ''
+    },
+
+    prerender: {
+      entries: ['/']
     }
   }
 };

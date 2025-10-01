04d0eb47321162e36ccca8e192657c22f13b093c
heunyam <tpwns072@gmail.com>
2025-10-02 06:40:48 +0900

    feat: add simple '/' page

--- me/src/routes/+layout.svelte
+++ me/src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script>
-	import '../app.css';  // ✅ 여기서 import
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
 </script>
 
-<slot />
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
---
+++ me/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="w-5xl p-10">
+	<h1 class="text-5xl">Hello, visitor.</h1>
+	<p class="text-4xl">Just some simple writings. Nothing fancy.</p>
+	<br />
+	<p class="text-3xl">
+		<span>1. </span>
+		<a class="text-blue-600 hover:underline" href="./book">Book</a>
+	</p>
+</div>

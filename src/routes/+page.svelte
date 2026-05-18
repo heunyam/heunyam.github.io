d0f42a4f562bdb1ca4f21c90fde94e9690338917
김세준 <tpwns072@gmail.com>
2026-05-18 17:35:15 +0800

    feat: Adds a sample Markdown file to the root page for testing (#2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import SampleMarkdown from '$lib/posts/샘플.md';
+</script>
 
+<main class="p-6">
+	<h1 class="mb-4 text-2xl font-bold">블로그 포스트 페이지</h1>
+	<article class="prose">
+		<SampleMarkdown />
+	</article>
+</main>

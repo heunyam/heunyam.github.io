f81b5e27d54312eb445044252faac5fb7c814a28
heunyam <tpwns072@gmail.com>
2026-05-22 14:04:48 +0800

    feat: add /release-notes routing

---
+++ src/routes/release-notes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import ReleaseNote from '$lib/release-notes.md';
+</script>
+
+<main class="p-12">
+	<h1 class="pb-4 text-2xl font-bold">Release Notes</h1>
+
+	<div class="flex flex-row">
+		<p class="pr-5">May 22, 2026</p>
+		<article class="prose">
+			<ReleaseNote />
+		</article>
+	</div>
+</main>

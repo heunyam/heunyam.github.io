ab78bdaf9bf85eb144616b14bc26d8c4d2c643b7
heunyam <tpwns072@gmail.com>
2026-05-28 02:31:57 +0800

    docs: add note v20260528

--- src/routes/release-notes/+page.svelte
+++ src/routes/release-notes/+page.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
-	import ReleaseNote from '$lib/release-notes.md';
+	import Note_0528 from '$lib/release-notes/20260528.md';
+	import Note_0522 from '$lib/release-notes/20260522.md';
 </script>
 
 <main class="p-12">
-	<h1 class="pb-4 text-2xl font-bold">Release Notes</h1>
+	<h1 class="pb-4 text-2xl font-medium">Release Notes</h1>
 
-	<div class="flex flex-row">
-		<p class="pr-5">May 22, 2026</p>
-		<article class="prose">
-			<ReleaseNote />
-		</article>
+	<div class="flex flex-col text-sm">
+		<div class="flex">
+			<time class="pr-5 pt-1">May 28, 2026</time>
+			<article class="prose">
+				<Note_0528 />
+			</article>
+		</div>
+		<div class="flex py-5">
+			<time class="pr-5 pt-1">May 22, 2026</time>
+			<article class="prose">
+				<Note_0522 />
+			</article>
+		</div>
 	</div>
 </main>

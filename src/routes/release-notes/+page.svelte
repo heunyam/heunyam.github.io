39e1c838fbc8336bc841d29a898da7e8736eb336
heunyam <tpwns072@gmail.com>
2026-06-01 05:57:59 +0800

    docs: 2026. 06. 01 기록 추가

--- src/routes/release-notes/+page.svelte
+++ src/routes/release-notes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Note_0601 from '$lib/release-notes/20260601.md';
 	import Note_0528 from '$lib/release-notes/20260528.md';
 	import Note_0522 from '$lib/release-notes/20260522.md';
 </script>
@@ -6,16 +7,22 @@
 <main class="p-12">
 	<h1 class="pb-4 text-2xl font-medium">Release Notes</h1>
 
-	<div class="flex flex-col text-sm">
-		<div class="flex">
-			<time class="pt-1 pr-5">May 28, 2026</time>
-			<article class="prose">
+	<div class="flex flex-col gap-10 text-sm">
+		<div class="flex gap-5">
+			<time>June 01, 2026</time>
+			<article class="prose text-[90%]">
+				<Note_0601 />
+			</article>
+		</div>
+		<div class="flex gap-5">
+			<time>May 28, 2026</time>
+			<article class="prose text-[90%]">
 				<Note_0528 />
 			</article>
 		</div>
-		<div class="flex py-5">
-			<time class="pt-1 pr-5">May 22, 2026</time>
-			<article class="prose">
+		<div class="flex gap-5">
+			<time>May 22, 2026</time>
+			<article class="prose text-[90%]">
 				<Note_0522 />
 			</article>
 		</div>

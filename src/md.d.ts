830e4708bf1734b5aa97d454b03a52e8a9c39bab
heunyam <tpwns072@gmail.com>
2026-06-08 13:49:20 +0800

    feat(blog): link the actual post page

--- src/md.d.ts
+++ src/md.d.ts
@@ -1,6 +1,14 @@
+type MarkdownMetadata = {
+	title?: string;
+	date?: string;
+	tags?: string[];
+};
+
 declare module '*.md' {
 	import type { Component } from 'svelte';
 
 	const component: Component;
 	export default component;
+
+	export const metadata: MarkdownMetadata;
 }

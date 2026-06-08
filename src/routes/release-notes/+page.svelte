6c3fbb1edcf967462f2e29c2852ea430f671e942
heunyam <tpwns072@gmail.com>
2026-06-08 13:50:59 +0800

    feat(release-notes): import all notes and rendering automatically

--- src/routes/release-notes/+page.svelte
+++ src/routes/release-notes/+page.svelte
@@ -1,30 +1,20 @@
 <script lang="ts">
-	import Note_0601 from '$lib/release-notes/20260601.md';
-	import Note_0528 from '$lib/release-notes/20260528.md';
-	import Note_0522 from '$lib/release-notes/20260522.md';
+	import { allNotes } from '$lib/utils/markdownLoader';
+
+	const noteReversed = allNotes.toReversed();
 </script>
 
 <main class="p-12">
 	<h1 class="pb-4 text-2xl font-medium">Release Notes</h1>
-
-	<div class="flex flex-col gap-10 text-sm">
-		<div class="flex gap-5">
-			<time>June 01, 2026</time>
-			<article class="prose text-[90%]">
-				<Note_0601 />
-			</article>
-		</div>
-		<div class="flex gap-5">
-			<time>May 28, 2026</time>
-			<article class="prose text-[90%]">
-				<Note_0528 />
-			</article>
-		</div>
-		<div class="flex gap-5">
-			<time>May 22, 2026</time>
-			<article class="prose text-[90%]">
-				<Note_0522 />
-			</article>
-		</div>
+	<div class="flex flex-col gap-4 text-sm">
+		{#each noteReversed as note (note.id)}
+			{@const Note = note.component}
+			<div class="flex max-w-170 gap-4 border-b-[0.5px]">
+				<time class="text-xs">{note.metadata.date}</time>
+				<article class="prose max-w-none text-[90%]">
+					<Note />
+				</article>
+			</div>
+		{/each}
 	</div>
 </main>

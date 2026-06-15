448f7c64178cf2628d4b112d530b1386342ef36a
김세준 <tpwns072@gmail.com>
2026-06-16 01:34:02 +0800

    feat(blog): refine the blog design (#16)

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -20,31 +20,32 @@
 	let PostContent = $derived(post?.component);
 </script>
 
-<div class="flex justify-center-safe md:mr-25">
+<div class="flex justify-center-safe">
 	<div class="flex w-200 flex-col pt-22">
-		<div id="post-header" class="mb-2 flex">
-			<div class="flex w-25 shrink-0 items-center justify-center">
-				<a href={blogMainURI} class="text-xs font-light text-ink-muted">돌아가기</a>
-			</div>
-			<div class="flex items-end pl-4">
-				<h1 class="text-xs">{postTitle}</h1>
+		<div class="flex pb-2 pl-4">
+			<div class="w-25"></div>
+			<div class="flex shrink-0 items-center justify-center">
+				<a href={blogMainURI} class="text-xs text-ink-muted">돌아가기</a>
 			</div>
 		</div>
 
 		<div id="post-body" class="flex border-y-[0.5px] border-line-bold">
-			<div class="w-25 shrink-0 border-r border-r-line-light">
-				<div class="flex items-center justify-center border-b-[0.5px] border-b-line-light py-1.5">
-					<time class="text-xs text-ink-muted">{postTime}</time>
+			<aside class="w-25 shrink-0">
+				<div class="border-r-[0.5px] border-line-light text-xs font-normal text-ink-muted">
+					<div class="text-primary border-y-[0.5px] border-line-light py-1.5 pl-4">
+						<time>{postTime}</time>
+					</div>
 				</div>
-			</div>
+			</aside>
 			<div class="max-w-175 grow">
-				<ul
-					class="flex items-center gap-1.5 border-b-[0.5px] border-b-line-light py-1.5 pl-5 text-xs font-light text-ink-primary"
-				>
-					{#each postTags as tag (tag)}
-						<li class="">#{tag}</li>
-					{/each}
-				</ul>
+				<div class="iterms-center flex gap-1.5 border-b-[0.5px] border-b-line-light py-1.5 pl-4">
+					<h1 class="text-xs text-ink-primary">{postTitle}</h1>
+					<ul class="flex items-center gap-1.5 text-xs font-light">
+						{#each postTags as tag (tag)}
+							<li class="">#{tag}</li>
+						{/each}
+					</ul>
+				</div>
 				<div class="prose max-w-none px-5 py-6 font-[Ridibatang] text-[75%]">
 					<PostContent />
 				</div>

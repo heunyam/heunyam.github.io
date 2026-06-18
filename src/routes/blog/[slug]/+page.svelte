69e53f1088e6d24327276bbaf68715313904904d
heunyam <tpwns072@gmail.com>
2026-06-19 00:30:07 +0800

    feat: apply design changes for mobile width

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,8 +2,10 @@
 	import { resolve } from '$app/paths';
 	import { allPosts } from '$lib/utils/markdownLoader';
 	import type { PageProps } from './$types';
+	import { innerWidth } from 'svelte/reactivity/window';
 
 	const blogMainURI = resolve('/blog');
+	const MOBILE_WIDTH = 480;
 
 	let { params }: PageProps = $props();
 	let slug = $derived(params.slug);
@@ -23,20 +25,24 @@
 <div class="flex justify-center-safe">
 	<div class="flex w-200 flex-col pt-22">
 		<div class="flex pb-2 pl-4">
-			<div class="w-25"></div>
+			{#if innerWidth.current && innerWidth.current > MOBILE_WIDTH}
+				<div class="w-25"></div>
+			{/if}
 			<div class="flex shrink-0 items-center justify-center">
 				<a href={blogMainURI} class="text-xs text-ink-muted">돌아가기</a>
 			</div>
 		</div>
 
 		<div id="post-body" class="flex border-y-[0.5px] border-line-bold">
-			<aside class="w-25 shrink-0">
-				<div class="border-r-[0.5px] border-line-light text-xs font-normal text-ink-muted">
-					<div class="text-primary border-y-[0.5px] border-line-light py-1.5 pl-4">
-						<time>{postTime}</time>
+			{#if innerWidth.current && innerWidth.current > MOBILE_WIDTH}
+				<aside class="w-25 shrink-0">
+					<div class="border-r-[0.5px] border-line-light text-xs font-normal text-ink-muted">
+						<div class="text-primary border-y-[0.5px] border-line-light py-1.5 pl-4">
+							<time>{postTime}</time>
+						</div>
 					</div>
-				</div>
-			</aside>
+				</aside>
+			{/if}
 			<div class="max-w-175 grow">
 				<div class="iterms-center flex gap-1.5 border-b-[0.5px] border-b-line-light py-1.5 pl-4">
 					<h1 class="text-xs text-ink-primary">{postTitle}</h1>
